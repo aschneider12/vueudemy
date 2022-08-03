049191f3586fd30adfb4030313ef090b8f89a9b8
ASchneider <schn.alessandro@gmail.com>
2022-08-03 17:45:21 -0400

    criada a primeira request

--- src/views/categories/CategoriesView.vue
+++ src/views/categories/CategoriesView.vue
@@ -1,15 +1,54 @@
 <template>
   <div>
-    <h1>Categorias</h1>
+    <div class="categories">
+      <h1>Categorias</h1>
+      <div class="list" v-for="category in categories" :key="category.id">
+
+        <div class="dados">{{category.id}}</div>
+        <div class="dados"> {{category.name}}</div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
+  data(){
+    return {
+      categories: {}
+    }
+  },
+  methods: {
+    getCategories(){
+      axios.get('http://localhost:8081/categories').then(resp => {
+        this.categories = resp.data
+      })
+    }
+  },
+  created(){
+    console.log('criou o categories')
+    this.getCategories();
+  }
 
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.categories {
+  align-items: center;
+  text-align: center;
+  .list{
+    align-items: center;
+    padding: 1%;
+    display: flex;
+    justify-content: center;
+    .dados {
+      padding: 0.1%;
+    }
+  }
+}
 </style>
--- src/views/products/ProductsView.vue
+++ src/views/products/ProductsView.vue
@@ -1,15 +1,90 @@
 <template>
   <div>
-    <h1>produtos</h1>
+    <h1>todos os produtos</h1>
+    <div class="principal">
+      <ul>
+        <li>
+          <div class="dados" id="id" ><strong>ID</strong>
+          </div>
+          <div class="dados" id="price"><strong>VALOR</strong>
+          </div>
+          <div class="dados" id="name"><strong>PRODUTO</strong>
+          </div>
+          <div class="dados" id="amount"> <strong>QTD</strong>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="principal" v-for="tupla in dados_local" :key="tupla.id">
+      <ul>
+        <li>
+          <div class="dados" id="id" >
+            {{ tupla.id }}
+          </div>
+          <div class="dados" id="price">
+            {{ dinheiro(tupla.price) }}
+          </div>
+          <div class="dados" id="name">
+            {{ tupla.name }}
+          </div>
+          <div class="dados" id="amount">
+            {{ tupla.amount }} un
+          </div>
+        </li>
+      </ul>
+    </div>
+    <hr />
+    <h1>somente categoria papelaria</h1>
+    <div class="mt-4" v-for="tupla in dados_local" :key="tupla.id">
+      <div v-if="tupla.category.name == 'Papelaria'">{{ tupla.name }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import banco from "@/data/data.json";
 
-}
+export default {
+  data() {
+    return {
+      dados_local: banco,
+    };
+  },
+  methods: {
+
+    dinheiro(valor) {
+
+        return 'R$'+valor.toFixed(2);
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.principal {
+    padding: 0.01%;
+    ul {
+        list-style-type: none;
+        li {
+            display: flex;
+            justify-content: space-around;
+            border: solid 1px gray;
+            padding: 1%;
+        }
+        .dados {
+            padding: 0.2%;
+            border-right: solid 1px gray;
+            border-left: solid 1px gray;
+        }
+        #id {
+            min-width: 5%;
+        }
+        #name {
+            min-width: 35%;
+        }
+        #amount, #price {
+            min-width: 15%;
+        }
+    }
+}
 </style>
