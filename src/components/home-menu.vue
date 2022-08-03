2ff3b8c9e119c6e34598d20b29fcffd19a0f25a4
ASchneider <schn.alessandro@gmail.com>
2022-08-03 15:10:08 -0400

    commit inicial do projeto

---
+++ src/components/home-menu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="conteudo">
+    <ul>
+      <router-link to="/">
+        <li>
+          <img class="imagem-brasao" src="@/assets/brasao_pc.png" alt="Course "/>
+        </li>
+      </router-link>
+      <router-link to="/">
+        <li>Início</li>
+      </router-link>
+      <router-link to="/about">
+        <li>Sobre</li>
+      </router-link>
+      <router-link to="/categories">
+        <li>Categorias</li>
+      </router-link>
+      <router-link to="products">
+        <li>Produtos</li>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeMenu",
+};
+</script>
+
+<style lang="scss" scoped>
+.conteudo {
+  ul {
+    justify-content: space-around;
+    display: flex;
+    list-style: none;
+    align-items: center;
+    li {
+      margin-left: 1%;
+      width: 100px;
+    }
+  }
+  .imagem-brasao {
+    width: 80px;
+  }
+}
+</style>
