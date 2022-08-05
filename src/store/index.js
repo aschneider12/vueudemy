f747f20cefdf0b6bf60cc0934ae0d1b25989e362
ASchneider <schn.alessandro@gmail.com>
2022-08-05 11:52:04 -0400

    alterando categories store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,73 @@
-import { createStore } from 'vuex'
+import { vuex } from 'vuex'
+import Axios from 'axios'
 
-export default createStore({
+const categoriesModule = {
   state: {
-  },
-  getters: {
+    categories: []
+  }
+}
+
+const productsModule = {
+  state: {
+    products: []
   },
   mutations: {
+    getProductsMutations(state, payload){
+      state.products = payload;
+    }
   },
   actions: {
-  },
+    getProductsAction( {commit}, payload) {
+      Axios.get('http://localhost:8081/products').then(response => {
+        
+        commit('getProductsMutations', response.data);
+      })
+    }
+  }
+
+}
+
+const store = createStore({
   modules: {
+    categoriesModule,
+    productsModule
   }
 })
+
+// ciclo do STORE VUEX
+// componenent -> chama uma action (processa, envia para a API )
+// export default createStore({
+//   state: {
+//     count:0,
+//     s_products: []
+//   },
+//   getters: {
+//   },
+//   mutations: {
+//     incrementar(state){
+//       console.log('chamou o incrementar')
+      // state.count++;
+//     },
+//     //qndo o action chamar/commitar o mutation, ele enviara para um render que atualiza a tela, esse e o ciclo conforme https://vuex.vuejs.org/#what-is-a-state-management-pattern
+//     mutationAddProduto(state, payload){
+//       console.log(payload, 'mutation')
+//       state.s_products.push(payload)
+//     }
+    
+//   },
+//   actions: {
+
+//     // a action inicia o update do estado, pode ser assincrona, qndo ela commitar ela chama o mutation, que vai refletir no render e ai na tela
+//     //nao sao chamadas diretamente as mutations, pois elas sao sincronas, ou seja, nao faz sentido usar 
+//     //uma arquitetura para ter o controle do estados dos componenttes e ficar chamando diretamente
+//     actionAddProduto( {commit}, payload){
+//       console.log(commit, 'context action')
+//       console.log(payload, 'action')
+    
+//       commit('mutationAddProduto', payload)
+//     }
+//   },
+//   modules: {
+
+//   }
+// })
--- src/views/categories/CategoriesView.vue
+++ src/views/categories/CategoriesView.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <div class="categories">
+      <div class="vuex">
+        {{count}}
+        <button v-on:click="incrementar">+ Clique</button>
+      </div>
+      <hr/>
+      <div class="class-produtos">
+        lista do products state: {{s_products}}
+        <input type="text" v-model="m_produtos"/>
+        <button v-on:click="actionAddProduto(m_produtos)" class="btn btn-primary mt-4">SUBMITER</button>
+      </div>
       <h1>Categorias</h1>
       <div class="list" v-for="category in categories" :key="category.id">
 
@@ -15,6 +25,7 @@
 <script>
 
 import axios from 'axios';
+import {mapState, mapMutations, mapActions} from 'vuex'
 
 export default {
   data(){
@@ -27,13 +38,26 @@ export default {
       axios.get('http://localhost:8081/categories').then(resp => {
         this.categories = resp.data
       })
-    }
+    },
+    //addCount(){
+     // this.$store.commit('incrementar')
+    //},
+    ...mapMutations(['incrementar']),
+
+    ...mapActions(['actionAddProduto'])
   },
   created(){
     console.log('criou o categories')
     this.getCategories();
-  }
+  },
+  computed:{
+    //contador(){
+    // return this.$store.state.count
+    //},
+    ...mapState(['count','s_products'])
 
+  }
+  
 }
 </script>
 
@@ -49,6 +73,21 @@ export default {
     .dados {
       padding: 0.1%;
     }
+  }
+}
+.vuex {
+  display: flex;
+  flex-direction: column;
+  font-size: 30px;
+  font-weight: 900;
+  align-items: center;
+}
+.class-produtos {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  input{
+    border-bottom: solid 1px black;
   }
 }
 </style>
